264abd9cc0eac776989e213ecb087c6bf5b492a3
Alejandra <maljagonzalez01@gmail.com>
2020-10-27 11:30:54 -0500

    registro de los usuarios

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,15 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCKC2mPPVfmmqq8BTrHZfVYJUzxXOScFSE",
-    authDomain: "web-mammamia.firebaseapp.com",
-    databaseURL: "https://web-mammamia.firebaseio.com",
-    projectId: "web-mammamia",
-    storageBucket: "web-mammamia.appspot.com",
-    messagingSenderId: "893011664005",
-    appId: "1:893011664005:web:abb7785b68aec55c669dec",
-    measurementId: "G-E570DWHD57"
-  };
 
-  firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
 
   const productsRef = db.collection('products');
---
+++ scripts/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKC2mPPVfmmqq8BTrHZfVYJUzxXOScFSE",
+    authDomain: "web-mammamia.firebaseapp.com",
+    databaseURL: "https://web-mammamia.firebaseio.com",
+    projectId: "web-mammamia",
+    storageBucket: "web-mammamia.appspot.com",
+    messagingSenderId: "893011664005",
+    appId: "1:893011664005:web:abb7785b68aec55c669dec",
+    measurementId: "G-E570DWHD57"
+  };
+
+  firebase.initializeApp(firebaseConfig);
