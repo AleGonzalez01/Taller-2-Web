3755c79ba525115f221048da75bef5c5362df97d
Alejandra <maljagonzalez01@gmail.com>
2020-11-17 15:42:07 -0500

    links

--- scripts/navegation.js
+++ scripts/navegation.js
@@ -2,8 +2,8 @@ const logo = document.querySelector('.navegation__image');
 const btn = document.querySelector('.navegation__sections button');
 
 logo.addEventListener('click', function (){
-  console.log("Buenas");
-    window.location.href='/taller-2-Web/';
+  console.log("GG");
+    window.location.href='/Taller-2-Web/';
   })
   
   btn.addEventListener('click', function (){
